b725e7ebb2d750b24c788d2926150b952df31248
patidarprasanna1602-sys <patidarprasanna1602@gmail.com>
2026-04-22 00:50:20 +0530

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,21 +1,27 @@
 import java.sql.*;
 
 public class DBConnection {
+    private static Connection con;
+
     public static Connection getConnection() {
         try {
-            Class.forName("org.postgresql.Driver");  // 🔥 ADD THIS
+            if (con == null || con.isClosed()) {
 
-            Connection con = DriverManager.getConnection(
-                "jdbc:postgresql://ep-frosty-cake-ao2fzuul-pooler.c-2.ap-southeast-1.aws.neon.tech:5432/neondb?sslmode=require",
-                "neondb_owner",
-                "npg_eHPJG2n1qxip"
-            );
+                Class.forName("org.postgresql.Driver");
 
-            System.out.println("Connected to Neon DB ");
+                con = DriverManager.getConnection(
+                    "jdbc:postgresql://ep-frosty-cake-ao2fzuul-pooler.c-2.ap-southeast-1.aws.neon.tech:5432/neondb?sslmode=require",
+                    "neondb_owner",
+                    "npg_eHPJG2n1qxip"
+                );
+
+                
+                System.out.println("Connected to Neon DB ");
+            }
+
             return con;
 
         } catch (Exception e) {
-            System.out.println("DB ERROR:");
             e.printStackTrace();
             return null;
         }
